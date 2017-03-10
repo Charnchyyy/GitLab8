48e971ff21a4047b9739eb177f7c71a7d0701f3a
firstics <phusith.sukt@gmail.com>
2017-03-10 19:05:51 +0700

    hanoi change by first

--- towerHanoiTurtle.py
+++ towerHanoiTurtle.py
@@ -1,26 +1,4 @@
 from turtle import *
-class Pole():
-    def __init__(self):
-
-        self.xPos = 0
-        self.yPos = 0
-
-    def showPole(self):
-        pu()
-        goto(self.xPos,self.yPos)
-        setheading(0)
-        pd()
-        for i in range(2):
-            fd(20)
-            left(90)
-            fd(100)
-            left(90)
-            
-        
-
-p = Pole()
-p.showPole()
-
 class disk:
     def __init__(self,name,x,y,height,width,color):
         self.name = name
@@ -63,6 +41,36 @@ class disk:
         lt(90)
         fd(self.width/2)
         end_fill()
+class Pole(disk):
+    def __init__(self,name,stack,top=0,xPos,yPos,thick,length,color):
+
+        self.name = name
+        self.stack = stack
+        self.top = top
+        self.xPos = xPos
+        self.yPos = yPos
+        self.thick = thick
+        self.length = length
+        self.color = color
         
+
+    def showPole(self):
+        pu()
+        goto(self.xPos,self.yPos)
+        setheading(0)
+        pd()
+        begin_fill()
+        color(str(self.color))
+        for i in range(2):
+            fd(self.thick)
+            left(90)
+            fd(self.length)
+            left(90)
+        end_fill()
+
+    def pushdisk(self,disk):
         
+    
         
+p = Pole()
+p.showPole()
